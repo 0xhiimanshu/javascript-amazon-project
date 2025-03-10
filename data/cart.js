dc961eefe7dbe4db8c1e354745013571efc35fe4
0xhiimanshu <himanshu.okoli@gmail.com>
2025-03-09 23:17:21 -0700

    cart change

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart = JSON.parse(localStorage.getItem("cart")) || [{
+export let cart = [{
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 1,
 }, {
@@ -35,8 +35,4 @@ export function removeFromCart(deletedItemId) {
   });
 
   cart = newCart;
-}
-
-function saveToStorage() {
-  localStorage.setItem("cart", JSON.stringify(cart));
 }
